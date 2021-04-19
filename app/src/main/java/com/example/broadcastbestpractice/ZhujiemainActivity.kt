a1cfbe3d0abf7d8fe3f3959fe88f38aefb154944
HanMo2021 <LR1778261936@gmail.com>
2021-04-20 01:14:55 +0800

    主界面设置
    隐藏主界面标题栏
    设置顶部状态栏为透明
    添加背景图片

--- app/src/main/java/com/example/broadcastbestpractice/ZhujiemainActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/ZhujiemainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.broadcastbestpractice
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
 
 class ZhujiemainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zhujiemain)
+
+        supportActionBar?.hide()//隐藏标题栏
+
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        //设置顶部状态栏为透明
     }
 }
