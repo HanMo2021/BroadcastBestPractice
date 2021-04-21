3ff23f9a2ed51ae1b7474bdcc7bab1d183f00999
HanMo2021 <LR1778261936@gmail.com>
2021-04-22 00:44:01 +0800

    完善框架
    完善框架

--- app/src/main/java/com/example/broadcastbestpractice/ShipingActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/ShipingActivity.kt
@@ -1,17 +1,23 @@
 package com.example.broadcastbestpractice
 
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_shiping.*
+import kotlinx.android.synthetic.main.activity_yinyue.*
 
 class ShipingActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shiping)
+
+
 
 
         supportActionBar?.hide()//隐藏标题栏
--- app/src/main/java/com/example/broadcastbestpractice/YinyueActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/YinyueActivity.kt
@@ -1,14 +1,27 @@
 package com.example.broadcastbestpractice
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_shiping.*
+import kotlinx.android.synthetic.main.activity_yinyue.*
+import kotlinx.android.synthetic.main.activity_zhujiemian.*
+import kotlinx.android.synthetic.main.activity_zhujiemian.img1
 
 class YinyueActivity : MobanActivity() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_yinyue)
+
+        img4.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://y.music.163.com/m/song/1832381575/?userid=272052686")
+            startActivity(intent)
+//给img4设计一个点击事件，点击后跳转到该网址
+        }
     }
 }
